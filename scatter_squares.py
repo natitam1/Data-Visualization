f7ac086d30e6baac3605530600d404a6637c15fa
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-12 15:07:50 +0300

    Save the graph in png file

--- scatter_squares.py
+++ scatter_squares.py
@@ -4,8 +4,7 @@ x_values = range(1, 1001)
 y_values = [x**2 for x in x_values]
 plt.style.use('_classic_test_patch')
 fig, ax = plt.subplots()
-
-ax.scatter(x_values, y_values, s=10)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Square Numbers", fontsize=24)
@@ -15,6 +14,6 @@ ax.set_ylabel("Square of Value", fontsize=14)
 # Set size of tick labels.
 ax.axis([0, 1100, 0, 1_100_000])
 ax.ticklabel_format(style = 'plain')
-ax.scatter(x_values, y_values, color=(0, 0.8, 0),s=10)
 
-plt.show()
+
+plt.savefig('squares_plot.png', bbox_inches='tight')
