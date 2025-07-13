cd35eb34e58a7e2fbafdd80d57f85de5a4805ce3
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-13 11:13:22 +0300

    Add blue color to the points on the graph

--- rw_visual.py
+++ rw_visual.py
@@ -10,7 +10,8 @@ while True:
     # Plot the points in the walk.
     plt.style.use('classic')
     fig, ax = plt.subplots()
-    ax.scatter(rw.x_values, rw.y_values, s=15)
+    point_numbers = range(rw.num_points)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
     ax.set_aspect('equal')
     plt.show()
 
