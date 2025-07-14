a00073625fd14fd66c1fe49795194e4de3767634
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-14 17:20:21 +0300

    Remove the styles

--- rw_visual.py
+++ rw_visual.py
@@ -8,7 +8,7 @@ while True:
     rw = RandomWalk(50_000)
     rw.fill_walk()
     # Plot the points in the walk.
-    plt.style.use('classic')
+    # plt.style.use('classic')
     fig, ax = plt.subplots(figsize=(15, 9), dpi=128)
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
