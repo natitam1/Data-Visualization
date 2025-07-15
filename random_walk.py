75862f6b332e267ffefac06050ccf710dee19bf3
Natnael Tamiru <natnaeltamiru98@gmail.com>
2025-07-15 10:14:45 +0300

    Create a separate method for random walk class

--- random_walk.py
+++ random_walk.py
@@ -10,20 +10,20 @@ class RandomWalk:
         # All walks start at (0, 0).
         self.x_values = [0]
         self.y_values = [0]
+
+    def get_step():
+        # Decide which direction to go, and how far to go.
+        direction = choice([1, -1])
+        distance = choice([0, 1, 2, 3, 4])
+        step = direction * distance
+        return step
     
     def fill_walk(self):
         """Calculate all the points in the walk."""
         # Keep taking steps until the walk reaches the desired length.
         while len(self.x_values) < self.num_points:
-
-            # Decide which direction to go, and how far to go.
-            x_direction = choice([1, -1])
-            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-            x_step = x_direction * x_distance
-
-            y_direction = choice([1, -1])
-            y_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-            y_step = y_direction * y_distance
+            x_step = self.get_step()
+            y_step = self.get_step()
 
             # Reject moves that go nowhere.
             if x_step == 0 and y_step == 0:
